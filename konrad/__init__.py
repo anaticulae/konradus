27cff141e7eaa4450db7667a54c7efc33fc1ddf3
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-05-26 19:15:06 +0200

    0.2.11 automatically generated release

--- konrad/__init__.py
+++ konrad/__init__.py
@@ -39,6 +39,6 @@ GERMAN = Language.GERMAN
 ENGLISH = Language.ENGLISH
 FRENCH = Language.FRENCH
 
-__version__ = '0.2.10'
+__version__ = '0.2.11'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
