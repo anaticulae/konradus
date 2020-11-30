1664c870e1f3adcddacb79efe6c0537bbe3fd288
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-11-30 20:28:35 +0100

    feat(abb): extend abbreviation list

--- konrad/abbreviation.py
+++ konrad/abbreviation.py
@@ -14,17 +14,20 @@ ABBREVIATION = {
     'Co.': '???',
     'Diss.': 'Dissertation',
     'Dok.': 'Dokument',  # ?
+    'Fig.': 'Figure',
     'Forts.': 'Fortsetzung',
-    'Hrsg.': 'Herausgeber',
     'H.': 'Heft',
+    'Hrsg.': 'Herausgeber',
     'Jg.': 'Jahrgang',
     'S.': 'Seite',
     'Sp.': 'Spalte',
+    'Tab.': 'Tabelle',
     'Verf.': 'Verfasser',
     'Verl.': 'Verlag',
     'Vol.': 'Volume',
     'a.a.O.': 'am angeführten Ort',
     'al.': '',
+    'bspw.': 'beispielsweise',
     'bzw.': 'beziehungsweise',
     'ca.': 'circa',
     'et. al.': 'et alii',  # TODO: CHECK DOTS
@@ -35,7 +38,7 @@ ABBREVIATION = {
     'ff.': '',
     'ggf.': 'gegebenenfalls',
     'lat.': 'latein',
-    'mind.': '',
+    'mind.': 'mindestens',
     'o.J.': 'ohne Jahrgangsangabe',
     'o.V.': 'ohne Verfasser',
     'o.Ä': 'ohne Änderung?',
@@ -43,9 +46,9 @@ ABBREVIATION = {
     'u.a.': 'unter anderem',
     'usw.': 'und so weiter',
     'vgl.': 'vergleiche',
-    'z.B.': 'zum Beispiel',
-    'z. B.': 'zum Beispiel',
     'vs.': 'versus',
+    'z. B.': 'zum Beispiel',
+    'z.B.': 'zum Beispiel',
 }
 
 ABBREVIATION_LOWER = {item.lower() for item in ABBREVIATION}
