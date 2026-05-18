004c652202abbf11fc9f1be2034184be764628fe
Helmut Konrad Schewe <helmutus@outlook.com>
2026-05-18 22:50:29 +0200

    chore(init): use dynamic version

--- konradus/__init__.py
+++ konradus/__init__.py
@@ -22,6 +22,7 @@ valid chunks.
 """
 
 import os
+from importlib.metadata import version as metaversion
 
 from konradus.abbr import ABBREVIATION
 from konradus.abbr import ABBREVIATION_LOWER
@@ -55,7 +56,7 @@ GERMAN = Language.GERMAN
 ENGLISH = Language.ENGLISH
 FRENCH = Language.FRENCH
 
-__version__ = '0.10.3'
+__version__ = metaversion('konradus')
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
